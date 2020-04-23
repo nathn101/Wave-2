ba8be64b5bd00b406809118546df5a214a5f9527
Nathan Chu <nathan.ch.chu@gmail.com>
2020-04-23 11:07:31 -0400

    first submission

---
+++ Month_to_days.py
@@ -0,0 +1,39 @@
+m = input("Enter a month: ")
+
+jan = "january"
+feb = "february"
+mar = "march"
+apr = "april"
+may = "may"
+jun = "june"
+jul = "july"
+aug = "august"
+sep = "september"
+oct = "october"
+nov = "november"
+dec = "december"
+
+if (m == jan):
+    print("There are 31 days in this month")
+if (m == mar):
+    print("There are 31 days in this month")
+if (m == may):
+    print("There are 31 days in this month")
+if (m == jul):
+    print("There are 31 days in this month")
+if (m == aug):
+    print("There are 31 days in this month")
+if (m == oct):
+    print("There are 31 days in this month")
+if (m == dec):
+    print("There are 31 days in this month")
+if (m == apr):
+    print("There are 30 days in this month")
+if (m == jun):
+    print("There are 30 days in this month")
+if (m == sep):
+    print("There are 30 days in this month")
+if (m == nov):
+    print("There are 30 days in this month")
+if(m == feb):
+    print("There are 28 or 29 days in this month")
