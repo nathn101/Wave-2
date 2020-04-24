8ad3e3334f0d9582bc3fe50a2a806ffc48017983
Nathan Chu <nathan.ch.chu@gmail.com>
2020-04-24 14:30:58 -0400

    first submission

---
+++ Colour_of_squares.py
@@ -0,0 +1,44 @@
+lINP = str(input("Enter letter position on the board(hit enter after letter): ")) 
+nINP = int(input("Enter a number position on the board: "))
+
+if (lINP == "a"):
+    if (nINP % 2 != 0):
+        print("black")
+    else:
+        print("white")
+if (lINP == "c"):
+    if (nINP % 2 != 0):
+        print("black")
+    else:
+        print("white")
+if (lINP == "e"):
+    if (nINP % 2 != 0):
+        print("black")
+    else:
+        print("white")
+if (lINP == "g"):
+    if (nINP % 2 != 0):
+        print("black")
+    else:
+        print("white")
+if (lINP == "b"):
+    if (nINP % 2 != 0):
+        print("white")
+    else:
+        print("black")
+if (lINP == "d"):
+    if (nINP % 2 != 0):
+        print("white")
+    else:
+        print("black")
+if (lINP == "f"):
+    if (nINP % 2 != 0):
+        print("white")
+    else:
+        print("black")
+if (lINP == "h"):
+    if (nINP % 2 != 0):
+        print("white")
+    else:
+        print("black")
+# attempted to use list, but didn't exactly know how to
